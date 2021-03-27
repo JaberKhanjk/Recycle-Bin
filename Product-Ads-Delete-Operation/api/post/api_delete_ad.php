08444acc46469625bbed92a77b9a8c78aadc5e5f
Jaber Khan <nahianrafi6@gmail.com>
2021-03-27 08:39:11 +0600

    deletion-api

---
+++ Product-Ads-Delete-Operation/api/post/api_delete_ad.php
@@ -0,0 +1,35 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    include_once '../../config/Database.php';
+    include_once '../../model/model_delete_ad.php';
+
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate blog post object
+    $post = new model_delete_ad($db);
+
+    // Get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+
+    if (isset($_POST["id"])){
+        $id = $_POST["id"];
+
+        if ($post->delete($id)){
+            
+            echo "true";
+        } else {
+            
+            echo "false";
+        }
+    }else{
+        echo "false";
+    }
+
+    
