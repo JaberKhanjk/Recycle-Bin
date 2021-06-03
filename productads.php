2fa562447bfb686d83f4a427e770dfd232ac9b59
Mehedi Hassan <mhassan171205@bscse.uiu.ac.bd>
2021-06-04 04:51:41 +0600

    Product Advertisement page created

---
+++ productads.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Search</title>
+    <link rel="stylesheet" href="css/semantic.min.css">
+</head>
+
+<body>
+    <div class="ui fluid menu">
+       
+        <div style="margin-left: 20px" class="header item">
+            Recycle Bin
+        </div>
+      <div class="right menu">
+        <a class="item active">Home</a>
+        <a  href="aboutus.php" class="item">About US</a>
+        <a class="item">Seller Dashboard</a>
+        <a class="item">Sign Up</a>
+        <a class="item">Sign In</a>
+        <a class="item">Log Out</a>
+      </div>
+    </div>
+ 
+    <div class="ui grid">
+        <div class="ui twelve wide column" style="background-image: url('../Recycle-Bin/back.jpg'); background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover; min-height: 100vh;">
+            <div style="padding: 20px">
+                <img style="height: 400px;width: 1000px; margin-left:auto;margin-right:auto;display:block " src="../Recycle-Bin/product1.jpg">
+            </div>
+            <div class="ui grid">
+                <div class="ui eight wide column">
+                  
+                  <p style="padding: 50px" class="center">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                </div>
+                <div class="ui eight wide column">
+                     <h1 style="padding: 100px" class="center">Product info</h1>
+                </div>
+
+               
+
+            </div>
+
+             <div style="padding:0 10% 0 10%" class="ui form">
+                  <div class="field">
+                    <labels>Comment:</label>
+                    <textarea></textarea>
+                  </div>
+                  <button style="margin-top: 10px" class="ui center floated fluid button" type="submit">Comment</button>
+                </div>
+            <div style="padding:30px">
+            </div>
+        </div>
+        <div class="ui four wide column" style="background-image: url('../Recycle-Bin/back2.jpg'); background-repeat: no-repeat ;background-size: cover; min-height: 100vh;">
+          <div style="padding: 20px" class="ui card">
+            <div class="image">
+              <img src="/Recycle-Bin/men.jpg">
+            </div>
+            <div class="content">
+              <a class="header">SAIDUL HAQUE ANIK</a>
+              <div class="meta">
+                <span class="date">Owner</span>
+              </div>
+              <div class="description">
+                He is an the owner of this product . Bla bla lorem ipsum we are wasting text yes thank you very much !
+              </div>
+
+            </div>
+            <div class="extra content">
+              <div class="ui star rating" data-rating="5"></div>
+            </div>
+          </div>
+
+        </div>
+    </div>
+
+
+
+    <!-- Rest of the page content -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/semantic.min.js"></script>
+</body>
+
+</html>
